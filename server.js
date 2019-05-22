0bd2f69e62e7c359172e8d05b5dba5082fd755d9
Josh Byster <joshb1050@gmail.com>
2019-05-22 02:23:46 -0500

    Finished the redis caching

--- server.js
+++ server.js
@@ -5,6 +5,7 @@ const redis = require("redis");
 const bluebird = require("bluebird");
 
 const API_URL = "https://developer.mtd.org/api/v2.2/json/";
+const CUMTD_API_KEY = process.env.CUMTD_API_KEY;
 const MAX_EXPECTED_MINS_AWAY = 60;
 
 // add the HTTP Keep-Alive header to speed up proxy requests
@@ -18,20 +19,49 @@ const opts = {
 const app = express();
 const port = 3000;
 
-client = redis.createClient();
+var client = redis.createClient();
 bluebird.promisifyAll(redis);
 
-app.get("/api/*", async (req, res) => {
-  let json = await fetch(
-    `${API_URL}/getdeparturesbystop?key=${CUMTD_API_KEY}&stop_id=PAR&pt=${MAX_EXPECTED_MINS_AWAY}`,
-    opts
-  ).then(res => res.json());
-  res.send(json);
+app.get("/api/getdeparturesbystop", async (req, res) => {
+  const { stop_id } = req.query;
+  if(!stop_id){
+      return res.send('invalid');
+  }
+  let stringifiedCurrentEntry = await client.getAsync(stop_id);
+  let resp;
+
+  if (stringifiedCurrentEntry) {
+    // cache hit!
+    const JSONCurrentEntry = JSON.parse(stringifiedCurrentEntry);
+    if (new Date() - new Date(JSONCurrentEntry.time) > 60 * 1000) {
+      // outdated, replace!
+      resp = await updateCache(stop_id);
+      resp.from_cache = false;
+    } else {
+      resp = JSONCurrentEntry;
+      resp.from_cache = true;
+    }
+  } else {
+    // missing from the cache store, add new entry
+    resp = await updateCache(stop_id);
+    resp.from_cache = false;
+  }
+  res.send(resp);
 });
 
-app.get("/redis", async (req,res) => {
-    let retval = await client.getAsync("string key"); 
-    res.send(retval);
+app.get("/api/*", async (req, res) => {
+    console.log("Hey!");
 });
+const updateCache = async stop_id => {
+  let json = await fetch(
+    `${API_URL}/getdeparturesbystop?key=${CUMTD_API_KEY}&stop_id=${stop_id}&pt=${MAX_EXPECTED_MINS_AWAY}`,
+    opts
+  ).then(res => res.json());
+
+  await client.setAsync(stop_id, JSON.stringify(json));
+  return json;
+};
+
+
 
 app.listen(port, () => console.log(`Express app listening on port ${port}!`));
